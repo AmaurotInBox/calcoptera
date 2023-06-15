4c85b4f8b8a10b295a8c181efa77476060cc2368
Nikolay Fomenko <nifomenko6610@gmail.com>
2023-06-15 22:03:03 +0300

    simple api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
 const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
 
-const app = express()
+const PORT = process.env.PORT || 4000
 
-const port = process.env.PORT || 4000
+const notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' },
+]
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!')
-})
+const typeDefs = gql`
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find((note) => note.id === args.id)
+    },
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      const noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott',
+      }
+      notes.push(noteValue)
+      return noteValue
+    },
+  },
+}
 
-app.listen(port, () => {
-  console.log(`Listening on http://127.0.0.1:${port}/`)
-  console.log(process.env)
-})
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({ typeDefs, resolvers })
+  const app = express()
+  await server.start()
+  server.applyMiddleware({ app, path: '/api' })
+
+  app.listen(PORT, () => {
+    console.log(
+      `Server is listening on port http://127.0.0.1:${PORT}${server.graphqlPath}`
+    )
+  })
+}
+
+startApolloServer(typeDefs, resolvers)
