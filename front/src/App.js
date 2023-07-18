9a38272a77532e5ad8417dfa41104e1638880ee0
Nikolay Fomenko <nifomenko6610@gmail.com>
2023-07-18 13:15:48 +0300

    api communication

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,22 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
 import Pages from './pages'
 import GlobalStyle from './components/GlobalStyle'
 
+const uri = process.env.API_URI
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true,
+})
+
 export default App = () => {
   return (
-    <>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Pages />
-    </>
+    </ApolloProvider>
   )
 }
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,11 +1,47 @@
 import React from 'react'
-import Button from '../components/Button'
+// import ReactMarkdown from 'react-markdown'
+import { useQuery, gql } from '@apollo/client'
+
+const GET_NOTES = gql`
+  query noteFeed($cursor: String) {
+    noteFeed(cursor: $cursor) {
+      cursor
+      hasNextPage
+      notes {
+        id
+        createdAt
+        content
+        favoriteCount
+        author {
+          username
+          id
+          avatar
+        }
+      }
+    }
+  }
+`
 
 export default Home = () => {
+  const { data, loading, error, fetchMore } = useQuery(GET_NOTES)
+
+  if (loading) return <p>loading...</p>
+
+  if (error) return <p>Error!</p>
+
   return (
-    <>
-      <h1>Hello world!</h1>
-      <Button>Click me!</Button>
-    </>
+    <div>
+      {data.noteFeed.notes.map((note) => (
+        <article key={note.id}>
+          <img
+            src={note.author.avatar}
+            alt={`${note.author.username} avatar`}
+            height="50px"
+          />{' '}
+          {note.author.username} {note.createdAt} {note.favoriteCount}{' '}
+          {/* <ReactMarkdown source={note.content} /> */}
+        </article>
+      ))}
+    </div>
   )
 }
